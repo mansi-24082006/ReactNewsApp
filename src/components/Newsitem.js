daa9e90aec48e5ee9879d31c780c506a5302a1f8
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-02-04 12:37:13 -0500

    Update Newsitem.js

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -4,16 +4,22 @@ export class Newsitem extends Component {
     let {title, description,Url,newsUrl,author,date,source} = this.props;
     return (
       <div className="my-3">
-       <div className="card" style={{width: "370px",height: "600px", objectFit: "cover"}}>
-            <img src={!Url?"https://unsplash.com/photos/a-bunch-of-balloons-that-are-shaped-like-email-7NT4EDSI5Ok":Url} className="card-img-top" style={{ width: "100%", height: "200px", objectFit: "cover" }} alt="..."/>
+       <div className="card">
+       <div style={{display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0'
+                  }}>
+       <span class=" badge rounded-pill bg-danger" >{source}</span></div>
+            <img src={!Url ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg":Url} className="card-img-top" style={{ width: "100%", height: "200px", objectFit: "cover" }} alt="..."/>
        <div className="card-body">
-            <h5 className="card-title">{title}<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{source}</span></h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}</p>
-            <p className="card-text"><small class="text-muted">By {!author?"Unknown": author} on {new Date(date).toGMTDString()}</small></p>
+            <p className="card-text"><small class="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target="_blank" rel="noreferrer" className ="btn btn-sm btn-dark">Read More</a>
        </div>
        </div>
-      </div>
+    </div>
     )
   }
 }
